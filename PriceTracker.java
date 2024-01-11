54b9bb5ba8f6c1e077801935727899b2eff86f82
shuheng0330 <thongshuheng0330@gmail.com>
2024-01-11 22:50:04 +0800

    first commit

---
+++ PriceTracker.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pricetracker;
+
+public class PriceTracker {
+
+    public static void main(String[] args) {
+        LoginForm1 LoginFrame=new LoginForm1();
+        LoginFrame.setVisible(true);
+        LoginFrame.pack();
+        LoginFrame.setLocationRelativeTo(null);
+    }
+    
+}
